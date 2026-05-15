ea08adf73c884c077dcf7db798da3d414e1c96b9
EandreaSanchez <erikaandrea.luna24@gmail.com>
2026-05-14 21:08:28 -0500

    update gift experience

--- app/page.tsx
+++ app/page.tsx
@@ -146,7 +146,7 @@ Este regalo fue obsequiado por Andrés y Erika.`;
               setTimeout(() => {
 
                 window.scrollTo({
-                  top: window.innerHeight,
+                  top: window.innerHeight * 0.92,
                   behavior:"smooth"
                 });
 
@@ -172,7 +172,7 @@ Este regalo fue obsequiado por Andrés y Erika.`;
             <motion.div
               initial={{ opacity:0, scale:.9 }}
               animate={{ opacity:1, scale:1 }}
-              transition={{ duration:.8 }}
+              transition={{ duration:4 }}
               className="w-full max-w-md mx-auto"
             >
 
